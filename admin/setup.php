e8a96663d193c7710cb3cc271db9dad640881c50
jpb <jean-pascal.boudet@atm-consulting>
2023-03-14 12:29:58 +0100

    création module et object notationNote ajout tab et sql ajout logo

--- admin/setup.php
+++ admin/setup.php
@@ -97,67 +97,13 @@ if (!class_exists('FormSetup')) {
 $formSetup = new FormSetup($db);
 
 
-// Hôte
-$item = $formSetup->newItem('NO_PARAM_JUST_TEXT');
-$item->fieldOverride = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'];
-$item->cssClass = 'minwidth500';
 
 // Setup conf NOTATION_MYPARAM1 as a simple string input
-$item = $formSetup->newItem('NOTATION_MYPARAM1');
-$item->defaultFieldValue = 'default value';
+$item = $formSetup->newItem('MIN_NOTATION');
+$item->defaultFieldValue = '0';
 
 // Setup conf NOTATION_MYPARAM1 as a simple textarea input but we replace the text of field title
-$item = $formSetup->newItem('NOTATION_MYPARAM2');
-$item->nameText = $item->getNameText().' more html text ';
-
-// Setup conf NOTATION_MYPARAM3
-$item = $formSetup->newItem('NOTATION_MYPARAM3');
-$item->setAsThirdpartyType();
-
-// Setup conf NOTATION_MYPARAM4 : exemple of quick define write style
-$formSetup->newItem('NOTATION_MYPARAM4')->setAsYesNo();
-
-// Setup conf NOTATION_MYPARAM5
-$formSetup->newItem('NOTATION_MYPARAM5')->setAsEmailTemplate('thirdparty');
-
-// Setup conf NOTATION_MYPARAM6
-$formSetup->newItem('NOTATION_MYPARAM6')->setAsSecureKey()->enabled = 0; // disabled
-
-// Setup conf NOTATION_MYPARAM7
-$formSetup->newItem('NOTATION_MYPARAM7')->setAsProduct();
-
-$formSetup->newItem('Title')->setAsTitle();
-
-// Setup conf NOTATION_MYPARAM8
-$item = $formSetup->newItem('NOTATION_MYPARAM8');
-$TField = array(
-	'test01' => $langs->trans('test01'),
-	'test02' => $langs->trans('test02'),
-	'test03' => $langs->trans('test03'),
-	'test04' => $langs->trans('test04'),
-	'test05' => $langs->trans('test05'),
-	'test06' => $langs->trans('test06'),
-);
-$item->setAsMultiSelect($TField);
-$item->helpText = $langs->transnoentities('NOTATION_MYPARAM8');
-
-
-// Setup conf NOTATION_MYPARAM9
-$formSetup->newItem('NOTATION_MYPARAM9')->setAsSelect($TField);
-
-
-// Setup conf NOTATION_MYPARAM10
-$item = $formSetup->newItem('NOTATION_MYPARAM10');
-$item->setAsColor();
-$item->defaultFieldValue = '#FF0000';
-$item->nameText = $item->getNameText().' more html text ';
-$item->fieldInputOverride = '';
-$item->helpText = $langs->transnoentities('AnHelpMessage');
-//$item->fieldValue = '';
-//$item->fieldAttr = array() ; // fields attribute only for compatible fields like input text
-//$item->fieldOverride = false; // set this var to override field output will override $fieldInputOverride and $fieldOutputOverride too
-//$item->fieldInputOverride = false; // set this var to override field input
-//$item->fieldOutputOverride = false; // set this var to override field output
+$item = $formSetup->newItem('MAX_NOTATION');
 
 
 $setupnotempty =+ count($formSetup->items);
